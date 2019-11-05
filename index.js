935a368ccb127d06accaee5c7d7aabb734d11634
David Hérault <dherault@gmail.com>
2019-11-05 10:53:45 +0100

    Add throttle function

---
+++ index.js
@@ -0,0 +1,42 @@
+function throttle(fn, delay) {
+  let timeoutId
+  const accumulator = []
+
+  const decumulate = () => {
+    accumulator.pop()
+    accumulator.forEach(fn => fn())
+    accumulator.length = 0
+  }
+
+  return (...args) => new Promise(resolve => {
+    clearTimeout(timeoutId)
+
+    accumulator.push(() => resolve({ hasResolved: false }))
+
+    const execute = () => {
+
+      const promiseOrValue = fn(...args)
+
+      if (typeof promiseOrValue.then === 'function') {
+        return promiseOrValue.then(value => {
+          decumulate()
+          resolve({
+            hasResolved: true,
+            value,
+          })
+        })
+      }
+
+      decumulate()
+
+      resolve({
+        hasResolved: true,
+        value: promiseOrValue,
+      })
+    }
+
+    timeoutId = setTimeout(execute, delay)
+  })
+}
+
+export default throttle
